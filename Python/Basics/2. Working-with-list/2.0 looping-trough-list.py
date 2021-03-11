8c0ca9c6b9d46748cf27d91f5bc6d55a6fba1308
Hasan Ahamed <officials.hasan@gmail.com>
2021-03-11 22:51:15 +0600

    refactor

---
+++ Python/Basics/2. Working-with-list/2.0 looping-trough-list.py
@@ -0,0 +1,11 @@
+magicians = [
+    'alice',
+    'david',
+    'caronline'
+]
+
+# looping throuh  items
+
+for magican in magicians:
+    print(magican.title()+ "was great magic bro")
+    print(magican.title()+ "good work bro")
